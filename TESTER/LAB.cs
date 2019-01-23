a990e4238d3313b28cd38d8dcd21d9ceb507d8b2
Bryan Martinez <martinez_o@hotmail.es>
2019-01-22 20:16:56 -0500

    Ok

---
+++ TESTER/LAB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTER
+{
+    public class Lab
+    {
+        public string TEST_Propiedades ()
+        {
+            return null;
+        }
+    }
+}
