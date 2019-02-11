bb39392c0acf1b140c9193afcd7a3bf03f032a3e
Bryan Martinez <martinez_o@hotmail.es>
2019-02-11 11:41:45 -0500

    Ok

--- UI/VentanaPpal.cs
+++ UI/VentanaPpal.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +10,11 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    public class Presentacion
+    public partial class VentanaPpal : Form
     {
-        public static void Main(string[] args)
+        public VentanaPpal()
         {
-            Application.Run(new VentanaPpal());
+            InitializeComponent();
         }
     }
 }
