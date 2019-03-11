0af2b8f8ef5da5238760792434accf922874d77c
Bryan Martinez <martinez_o@hotmail.es>
2019-03-10 22:40:06 -0500

    MONOMIOS FUNCIONANDO, AVANCE EN POLINOMIOS, FALTA CORREGIR ALGUNOS BUGS EN POLINOMIOS.

--- ALGEBRA/Polinomios.cs
+++ ALGEBRA/Polinomios.cs
@@ -13,8 +13,8 @@ namespace ALGEBRA
         public string Expresion { get; private set;}
         public List<Monomios> Polinomio { get; private set; }
         private Monomios Monomio;
-        private AMathOps Suma = new Sumas();
-        private AMathOps Resta = new Sustracciones();
+        Sumas Suma = new Sumas();
+        Sustracciones Resta = new Sustracciones();
 
         public Polinomios(List<Monomios> Polinomio)
         {
@@ -57,4 +57,85 @@ namespace ALGEBRA
             }
         }
     }
+
+    public class CopPolinomios
+    {
+        public string Nombre => "POLINOMIO";
+        public string Contenido { get; private set; }
+        public string Result { get; private set; }
+        public double GradoAbs { get; private set; }
+        public List<Monomios> Elementos = new List<Monomios>();
+        Monomios Monomio;
+        SumaEntera Suma = new SumaEntera();
+
+        EProcesos Proceso = new EProcesos();
+
+        public CopPolinomios() { }
+
+        public CopPolinomios(string Expresion)
+        {
+            if (Proceso.IsAgrupate(Expresion))
+                Expresion = Proceso.DescorcharA(Expresion);
+
+            ObtenerElementos(Expresion);
+
+            Operar();
+        }
+
+        private void ObtenerElementos(string Expresion)
+        {
+            GradoAbs = 0;
+            Elementos.Clear();
+            Contenido = "";
+            Suma = new SumaEntera(Expresion);
+
+            Contenido = Suma.Result;
+
+            foreach (var elemento in Contenido.Split(Suma.Simbolo))
+            {
+                Monomio = new Monomios(elemento);
+
+                if (!Monomio.Result.Equals(Suma.Modulo))
+                {
+                    Elementos.Add(Monomio);
+                    GradoAbs += Monomio.GradoAbs;
+                }
+
+            }
+
+            Contenido = Contenido.Trim(Suma.Simbolo);
+
+        }
+
+        private void Operar()
+        {
+            Result = "";
+            string Temporal;
+            bool A;
+
+            A = Elementos.Count < 1;
+
+            if (A)
+                Result = $"{Suma.Modulo}";
+            else
+            {
+                foreach (var M in Elementos)
+                {
+                    Temporal = M.Result;
+                    A = Temporal.Length > 3;
+
+                    if (A)
+                    {
+                        Temporal = Proceso.EncorcharParentesis(Temporal);
+                    }
+
+                    Result += Suma.Simbolo + Temporal;
+                }
+
+                Result = Result.Trim(Suma.Simbolo);
+            }
+        }
+
+    }
+
 }
---
+++ TESTER/TestAlgebra.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITY;
+using ALGEBRA;
+
+namespace TESTER
+{
+    public class TestAlgebra
+    {
+        public static void Main(string[] args)
+        {
+            TEST_Consola_Polinomio();
+        }
+
+        private static void TEST_Consola_Moniomio()
+        {
+            string Entrada = "y^{2*4}*x*2*z*5";
+            Monomios MONO = new Monomios(Entrada);
+            Console.WriteLine(MONO.Nombre);
+            Console.WriteLine($"ENTRADA = {Entrada}");
+            Console.WriteLine("CONTENIDO   " + MONO.Contenido);
+            Console.WriteLine("COEFICIENTE " + MONO.Coeficiente);
+            Console.WriteLine("LITERAL     " + MONO.ParteLiteral);
+            Console.WriteLine("GRADO ABS   " + MONO.GradoAbs);
+            Console.WriteLine(MONO.Result);
+            Console.WriteLine("-----------------------------------");
+            Console.ReadKey();
+        }
+
+        private static void TEST_Consola_Polinomio()
+        {
+            string Entrada = "x*2+(2)+(y^{2*4}*x*2*z*5)";
+            CopPolinomios POLI = new CopPolinomios(Entrada);
+            Console.WriteLine(POLI.Nombre);
+            Console.WriteLine($"ENTRADA = {Entrada}");
+            Console.WriteLine("CONTENIDO   " + POLI.Contenido);
+            Console.WriteLine("GRADO ABS   " + POLI.GradoAbs);
+            Console.WriteLine(POLI.Result);
+            Console.WriteLine("-----------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
