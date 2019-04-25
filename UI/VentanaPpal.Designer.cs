f59cb85e942e0b9b566acc0a4bcdd7c7da6c4d60
Bryan Martinez <martinez_o@hotmail.es>
2019-04-25 08:11:32 -0500

    Visual 2019

--- UI/VentanaPpal.Designer.cs
+++ UI/VentanaPpal.Designer.cs
@@ -396,9 +396,9 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(674, 473);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.Size = new System.Drawing.Size(169, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Design by Ing. Brayan D. Martinez";
+            this.label1.Text = " Design by Brayan D. Martinez";
             // 
             // pictureBox2
             // 
